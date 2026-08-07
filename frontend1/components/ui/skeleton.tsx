faf9866f1b3d8f0f91f64a1b646e7f1b46026514
ThaiQuangHuy2906 <24127177@student.hcmus.edu.vn>
2026-08-07 17:40:46 +0700

    Remove draft frontend1 and track raw data

--- frontend1/components/ui/skeleton.tsx
+++
@@ -1,7 +0,0 @@
-import { cn } from "@/lib/utils";
-
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn("animate-pulse rounded-lg bg-surface-border/60", className)} {...props} />;
-}
-
-export { Skeleton };
